c6bbfaff845bd8d81d6e3670db8fa1db8a736212
chendumpling99 <66714443+chendumpling99@users.noreply.github.com>
2022-05-24 22:55:09 -0700

    2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,15 +2,16 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '123-456-7890'
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
 
   // controls form input element
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState(persons)
 
   const addName = (event) => {
     event.preventDefault();
@@ -35,9 +36,27 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleSearch = (event) => {
+    const searchWord = event.target.value.toLowerCase()
+    const newFilter = persons.filter(person => (
+      person.name.toLowerCase().includes(searchWord)
+    ))
+    setFilter(newFilter)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        <p>
+          filter shown with <input
+            id='searchName'
+            placeholder='Search name...'
+            onChange={handleSearch}
+          />
+        </p>
+      </div>
+      <h2>Add new contact</h2>
       <form onSubmit={addName}>
         <div>
           name: <input
@@ -56,9 +75,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
-        {persons.map(person => (
-          <li key={person.name}>{person.name} {person.newNumber}</li>
+      <ul id="filterList">
+        {filter.map(person => (
+          <li key={person.id}>{person.name} {person.number}</li>
         ))}
       </ul>
     </div>
